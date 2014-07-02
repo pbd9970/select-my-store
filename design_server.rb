1a93883069a75d5897f279faf7bc9994685cee95
Lindsey Lee <lb.lee3@yahoo.com>
2014-07-02 17:41:00 -0500

    add images for logos, create skeleton for views

--- design_server.rb
+++ design_server.rb
@@ -7,8 +7,19 @@ require_relative 'lib/sms.rb'
 set :bind, '0.0.0.0'
 set :port, 9494
 
-get '/design/home' do
+get '/design' do
   @message = "im data"
   erb :index
 end
 
+get '/design/home' do
+  erb :home
+end
+
+get '/design/results' do
+  erb :results
+end
+
+get '/design/qualities' do
+  erb :qualities
+end
