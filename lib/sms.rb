df79379c4f5feaedb4a825e5b7a8afe7b0e8a5b1
Lindsey Lee <lb.lee3@yahoo.com>
2014-07-03 09:11:20 -0500

    updates to design

--- lib/sms.rb
+++ lib/sms.rb
@@ -1,28 +1,26 @@
-# module SMS
-#   def db
-#     @__db_instance ||= DB.new
-#   end
+module SMS
+  def db
+    @__db_instance ||= DB.new
+  end
 
-<<<<<<< HEAD
-# module SMS
-#   def db
-#     @__db_instance ||= DB.new
-#   end
+module SMS
+  def db
+    @__db_instance ||= DB.new
+  end
 
-# require 'sinatra'
-# require 'pg'
-# require 'digest'
-# require 'securerandom'
+require 'sinatra'
+require 'pg'
+require 'digest'
+require 'securerandom'
 
-=======
-# require 'sinatra'
-# require 'pg'
-# require 'digest'
-# require 'securerandom'
+require 'sinatra'
+require 'pg'
+require 'digest'
+require 'securerandom'
 
->>>>>>> lindsey
-# require-relative 'lib/user.rb'
-# require-relative 'lib/store.rb'
-# require-relative 'lib/quality.rb'
-# require-relative 'lib/db.rb'
-# require-relative "lib/scripts/*.rb"
+
+require-relative 'lib/user.rb'
+require-relative 'lib/store.rb'
+require-relative 'lib/quality.rb'
+require-relative 'lib/db.rb'
+require-relative "lib/scripts/*.rb"
